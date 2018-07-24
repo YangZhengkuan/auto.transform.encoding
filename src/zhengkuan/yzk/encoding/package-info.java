fff93836b42576fff58aaea5d632d7e7cc789788
zhengkuan.yzk <1329750315@qq.com>
2018-07-24 20:30:28 +0800

    delete all files

--- src/zhengkuan/yzk/encoding/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * 文件编码处理工具类
- *
- * @author zhengkuan.yzk@alibaba-inc.com
- * @date 2018/7/8
- */
-package zhengkuan.yzk.encoding;
--- src/zhengkuan/yzk/handler/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * 监听编辑器中文件的改变，当文件切换时，进行文件编码的识别和转换
- *
- * @author zhengkuan.yzk@alibaba-inc.com
- * @date 2018/7/8
- */
-package zhengkuan.yzk.handler;
